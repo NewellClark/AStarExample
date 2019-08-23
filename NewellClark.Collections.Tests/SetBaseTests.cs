7c001509ed3cd03bd813d483d08a0b86404f5749
NewellClark <newellofthefuture@gmail.com>
2019-08-23 16:15:57 -0400

    - Added SetBase<T> and SetWrapper<T>

---
+++ NewellClark.Collections.Tests/SetBaseTests.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NewellClark.DataStructures.Collections.Tests
+{
+	[TestFixture]
+	public abstract class SetTests
+	{
+		protected abstract ISet<T> Create<T>();
+
+		protected virtual ISet<T> Create<T>(IEnumerable<T> items)
+		{
+			var result = Create<T>();
+			foreach (var item in items)
+				result.Add(item);
+
+			return result;
+		}
+
+		private IEnumerable<T> Sequence<T>(params T[] items) => items.AsEnumerable();
+
+		private void AddAll<T>(ISet<T> set, params T[] items)
+		{
+			foreach (var item in items)
+				set.Add(item);
+		}
+
+		private ISet<int> set;
+
+		[SetUp]
+		public void Initialize() => set = Create<int>();
+
+		[Test]
+		public void Add_WillNotAddDuplicates()
+		{
+			AddAll(set, 3, 3);
+
+			var expected = Sequence(3);
+			var actual = set;
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void Add_TrueWhenAbsent()
+		{
+			Assert.True(set.Add(1));
+		}
+
+		[Test]
+		public void Add_FalseWhenPresent()
+		{
+			set.Add(1);
+			Assert.False(set.Add(1));
+		}
+
+		[Test]
+		public void Remove_RemovesSpecifiedElement()
+		{
+			AddAll(set, 3, 7, 14);
+			set.Remove(7);
+
+			var expected = Sequence(3, 14);
+			var actual = set;
+
+			CollectionAssert.AreEquivalent(expected, actual);
+		}
+
+		[Test]
+		public void Remove_TrueWhenPresent()
+		{
+			AddAll(set, 5, 3, -2);
+			Assert.True(set.Remove(-2));
+		}
+
+		[Test]
+		public void Remove_FalseWhenAbsent()
+		{
+			Assert.False(set.Remove(15));
+		}
+
+		[Test]
+		public void Clear_RemovesAllItems()
+		{
+			AddAll(set, 0, 1, 2, 3, 4, 5);
+
+			Assume.That(set.Count == 6);
+
+			set.Clear();
+
+			CollectionAssert.IsEmpty(set);
+		}
+
+		[Test]
+		public void Contains_TrueWhenPresent()
+		{
+			set.Add(5);
+			set.Add(3);
+			Assert.True(set.Contains(5));
+		}
+
+		[Test]
+		public void Contains_FalseWhenAbsent()
+		{
+			AddAll(set, 4, 16, 3);
+			Assert.False(set.Contains(17));
+		}
+
+		[Test]
+		public void Count_EmptySet_IsZero()
+		{
+			Assert.AreEqual(0, set.Count);
+		}
+
+		[Test]
+		public void Count_KeepsTrackOfAddsAndRemoves()
+		{
+			AddAll(set, 4, 3, 7, 7, 5);     //	4
+			set.Remove(7);                  //	3
+			set.Remove(5);                  //	2
+			set.Remove(4);                  //	1
+
+			Assert.AreEqual(1, set.Count);
+		}
+
+		[Test]
+		public void Enumerator_IteratesAllItems()
+		{
+			AddAll(set, 3, 7, 6, 6, 4, 3, -7);
+			var expected = Sequence(3, 7, 6, 4, -7);
+			var actual = set.AsEnumerable();
+
+			CollectionAssert.AreEquivalent(expected, actual);
+		}
+
+		[Test]
+		public void CopyTo_JustEnoughRoom_Succeeds()
+		{
+			AddAll(set, 4, 5, 6, 7);
+			var array = new int[7];
+			array[0] = 1;
+			array[1] = 2;
+			array[2] = 3;
+
+			set.CopyTo(array, 3);
+
+			var expected = Sequence(1, 2, 3, 4, 5, 6, 7);
+
+			CollectionAssert.AreEquivalent(expected, array);
+		}
+
+		[Test]
+		public void CopyTo_NegativeIndex_Throws()
+		{
+			void test() => set.CopyTo(new int[1], -1);
+
+			Assert.Throws<ArgumentOutOfRangeException>(test);
+		}
+
+		[Test]
+		public void CopyTo_NotEnoughRoom_Throws()
+		{
+			AddAll(set, 1, 2, 3);
+			var array = new int[5];
+
+			void test() => set.CopyTo(array, 3);
+
+			Assert.Throws<ArgumentOutOfRangeException>(test);
+		}
+
+		[Test]
+		public void CopyTo_NullArray_Throws()
+		{
+			void test() => set.CopyTo(null, 0);
+
+			Assert.Throws<ArgumentNullException>(test);
+		}
+
+
+		[Test]
+		public void ExceptWith_EmptySet_DoesNothing()
+		{
+			AddAll(set, 5, 3, 4, 2);
+			var expected = set.ToArray();
+
+			set.ExceptWith(Sequence<int>());
+
+			CollectionAssert.AreEquivalent(expected, set);
+		}
+
+		[Test]
+		public void ExceptWith_RemovesOtherElements()
+		{
+			AddAll(set, 5, 4, 3, 2, 1);
+			var other = Sequence(4, 2, 1, 6);
+			var expected = Sequence(5, 3);
+
+			set.ExceptWith(other);
+
+			CollectionAssert.AreEquivalent(expected, set);
+		}
+
+		[Test]
+		public void ExceptWith_Self_RemovesAllItems()
+		{
+			AddAll(set, 1, 2, 3, 4);
+
+			Assume.That(set.Count == 4);
+
+			set.ExceptWith(set);
+
+			CollectionAssert.IsEmpty(set);
+		}
+
+		[Test]
+		public void SymmetricExceptWith_ActsAsExclusiveOr()
+		{
+			AddAll(set, 1, 2, 3, 4, 5, 6);
+			var other = Sequence(4, 5, 6, 7, 8, 9);
+			var expected = Sequence(1, 2, 3, 7, 8, 9);
+
+			set.SymmetricExceptWith(other);
+
+			CollectionAssert.AreEquivalent(expected, set);
+		}
+
+		[Test]
+		public void SymmetricExceptWith_Self_RemovesAllItems()
+		{
+			AddAll(set, 1, 2, 3, 4);
+
+			Assume.That(set.Count == 4);
+
+			set.SymmetricExceptWith(set);
+
+			CollectionAssert.IsEmpty(set);
+		}
+
+		[Test]
+		public void IntersectWith_RemovesElementsNotInOther()
+		{
+			AddAll(set, 1, 2, 3, 4, 5, 6);
+			var other = Sequence(3, 4, 5, 17, -2);
+			var expected = Sequence(3, 4, 5);
+
+			set.IntersectWith(other);
+
+			CollectionAssert.AreEquivalent(expected, set);
+		}
+
+		[Test]
+		public void IntersectWith_Self_DoesNothing()
+		{
+			AddAll(set, 1, 2, 3, 4);
+			var before = set.ToArray();
+
+			set.IntersectWith(set);
+
+			CollectionAssert.AreEquivalent(before, set);
+		}
+
+		[Test]
+		public void UnionWith_AddsMissingElements()
+		{
+			AddAll(set, 1, 2, 3, 4, 5, 6);
+			var other = Sequence(-1, 0, 1, 2, 0, 17, 17, 18, 19);
+			var expected = Sequence(-1, 0, 1, 2, 3, 4, 5, 6, 17, 18, 19);
+
+			set.UnionWith(other);
+
+			CollectionAssert.AreEquivalent(expected, set);
+		}
+
+		[Test]
+		public void UnionWith_Self_DoesNothing()
+		{
+			AddAll(set, 1, 2, 3, 4);
+			var before = set.ToArray();
+
+			set.UnionWith(set);
+
+			CollectionAssert.AreEquivalent(before, set);
+		}
+
+		[Test]
+		public void SetEquals_IgnoresDuplicates()
+		{
+			AddAll(set, 1, 2, 3, 4, 5);
+			var other = Sequence(1, 2, 2, 3, 4, 4, 4, 5, 5);
+
+			Assert.True(set.SetEquals(other));
+		}
+
+		[Test]
+		public void SetEquals_IgnoresOrder()
+		{
+			AddAll(set, 1, 2, 3, 4, 5);
+			var other = Sequence(5, 4, 1, 3, 2);
+
+			Assert.True(set.SetEquals(other));
+		}
+
+		[Test]
+		public void EmptySet_IsSubsetOfEmptySet()
+		{
+			var other = Sequence<int>();
+
+			Assert.True(set.IsSubsetOf(other));
+		}
+
+		[Test]
+		public void EmptySet_IsNotProperSubsetOfEmptySet()
+		{
+			var other = Sequence<int>();
+			Assert.False(set.IsProperSubsetOf(other));
+		}
+
+		[Test]
+		public void IsSubsetOf_EqualSets_True()
+		{
+			AddAll(set, 1, 2, 3, 4, 5);
+			var other = Sequence(1, 2, 3, 4, 5);
+
+			Assert.True(set.IsSubsetOf(other));
+		}
+
+		[Test]
+		public void IsSubsetOf_SingleMissingElement_False()
+		{
+			AddAll(set, 1, 2, 4, 5);
+			var other = Sequence(1, 2, 3);
+
+			Assert.False(set.IsSubsetOf(other));
+		}
+
+		[Test]
+		public void IsProperSubsetOf_EqualSets_False()
+		{
+			AddAll(set, 1, 2, 3, 4, 5);
+			var other = Sequence(1, 2, 3, 4, 5);
+
+			Assert.False(set.IsProperSubsetOf(other));
+		}
+
+		[Test]
+		public void IsProperSubsetOf_OneExtraElement_True()
+		{
+			AddAll(set, 1, 2, 3);
+			var other = Sequence(0, 1, 2, 3);
+
+			Assert.True(set.IsProperSubsetOf(other));
+		}
+
+		[Test]
+		public void EmptySet_IsSupersetOfEmptySet()
+		{
+			Assert.True(set.IsSupersetOf(Sequence<int>()));
+		}
+
+		[Test]
+		public void IsSupersetOf_EqualSets_True()
+		{
+			AddAll(set, 1, 2, 3);
+			var other = Sequence(1, 3, 2);
+
+			Assert.True(set.IsSupersetOf(other));
+		}
+
+		[Test]
+		public void IsProperSupersetOf_EqualSets_False()
+		{
+			AddAll(set, 1, 2, 3);
+			var other = Sequence(3, 2, 1);
+
+			Assert.False(set.IsProperSupersetOf(other));
+		}
+
+		[Test]
+		public void IsProperSupersetOf_OneExtra_True()
+		{
+			AddAll(set, 0, 1, 2, 3);
+			var other = Sequence(1, 3, 2);
+
+			Assert.True(set.IsProperSupersetOf(other));
+		}
+
+		[Test]
+		public void Overlaps_OneInCommon_True() 
+		{
+			AddAll(set, 0, 1, 2, 3);
+			var other = Sequence(3, 4, 5, 6);
+
+			Assert.True(set.Overlaps(other));
+		}
+
+		[Test]
+		public void Overlaps_NoneInCommon_False()
+		{
+			AddAll(set, 0, 1, 2, 3);
+			var other = Sequence(4, 5, 6, 7);
+
+			Assert.False(set.Overlaps(other));
+		}
+	}
+
+	[TestFixture]
+	public class SetWrapperTests : SetTests
+	{
+		protected override ISet<T> Create<T>() => new SetWrapper<T>();
+	}
+}
---
+++ NewellClark.Collections/SetBase.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewellClark.DataStructures.Collections
+{
+
+	public abstract class SetBase<T> : ISet<T>
+	{
+		protected SetBase() { }
+
+		public bool Add(T item) => AddItem(item);
+
+		public bool Remove(T item) => RemoveItem(item);
+
+		public bool Contains(T item) => ContainsItem(item);
+
+		public void Clear() => ClearItems();
+
+		protected abstract bool AddItem(T item);
+
+		protected abstract bool RemoveItem(T item);
+
+		protected abstract bool ContainsItem(T item);
+
+		protected abstract void ClearItems();
+
+		/// <summary>
+		/// Gets the <see cref="IEqualityComparer{T}"/> that is used to determine item equality.
+		/// </summary>
+		public abstract IEqualityComparer<T> Comparer { get; }
+
+		public abstract IEnumerator<T> GetEnumerator();
+
+		public abstract int Count { get; }
+
+		public abstract bool IsReadOnly { get; }
+
+		/// <summary>
+		/// Copies the current <see cref="SetBase{T}"/> to the specified array, starting at the
+		/// specified index in the array.
+		/// </summary>
+		/// <param name="array">The array to copy to.</param>
+		/// <param name="arrayIndex">The index to start copying at.</param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="array"/> is null.
+		/// </exception>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <paramref name="arrayIndex"/> is negative, or the array doesn't have the capacity
+		/// to hold entire collection starting at the specified index.
+		/// </exception>
+		public virtual void CopyTo(T[] array, int arrayIndex)
+		{
+			if (array is null) throw new ArgumentNullException(nameof(array));
+			if (arrayIndex < 0) throw new ArgumentOutOfRangeException(nameof(arrayIndex), $"Can't be negative.");
+			if (arrayIndex + Count > array.Length)
+				throw new ArgumentOutOfRangeException(nameof(arrayIndex), $"Not enough room in array to copy collection.");
+
+			using (var iter = GetEnumerator())
+			{
+				for (int index = arrayIndex; iter.MoveNext(); ++index)
+				{
+					array[index] = iter.Current;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Removes all items that match the specified <see cref="Predicate{T}"/>. 
+		/// </summary>
+		/// <param name="predicate">Predicate to determine whether an item should be removed.</param>
+		/// <returns>The number of items that were removed.</returns>
+		public virtual int RemoveWhere(Predicate<T> predicate)
+		{
+			if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+
+			var remove = this.Where(i => predicate(i)).ToArray();
+			foreach (T item in remove)
+				Remove(item);
+
+			return remove.Length;
+		}
+
+
+		public virtual void ExceptWith(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			if (other == this)
+			{
+				Clear();
+				return;
+			}
+
+			foreach (var item in other)
+				Remove(item);
+		}
+
+		public virtual void SymmetricExceptWith(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			if (other == this)
+			{
+				Clear();
+				return;
+			}
+
+			foreach (T item in other.Distinct(Comparer))
+			{
+				if (!Remove(item))
+					Add(item);
+			}
+		}
+
+		public virtual void IntersectWith(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			if (other == this)
+				return;
+
+			var theirs = new HashSet<T>(other, Comparer);
+
+			RemoveWhere(i => !theirs.Contains(i));
+		}
+
+		public virtual void UnionWith(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			if (other == this)
+				return;
+
+			foreach (T item in other)
+				Add(item);
+		}
+
+		public virtual bool SetEquals(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			foreach (T item in other)
+				if (!Contains(item))
+					return false;
+
+			return true;
+		}
+
+		public virtual bool Overlaps(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			foreach (T item in other)
+				if (Contains(item))
+					return true;
+
+			return false;
+		}
+
+		public virtual bool IsSubsetOf(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			var theirs = new HashSet<T>(other, Comparer);
+
+			if (Count > theirs.Count)
+				return false;
+
+			foreach (T item in this)
+				if (!theirs.Contains(item))
+					return false;
+
+			return true;
+		}
+
+		public virtual bool IsProperSubsetOf(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			var theirs = new HashSet<T>(other, Comparer);
+
+			if (Count >= theirs.Count)
+				return false;
+
+			foreach (T item in this)
+				if (!theirs.Contains(item))
+					return false;
+
+			return true;
+		}
+
+		public virtual bool IsSupersetOf(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			var theirs = new HashSet<T>(other, Comparer);
+
+			if (Count < theirs.Count)
+				return false;
+
+			foreach (T item in theirs)
+				if (!Contains(item))
+					return false;
+
+			return true;
+		}
+
+		public virtual bool IsProperSupersetOf(IEnumerable<T> other)
+		{
+			if (other is null) throw new ArgumentNullException(nameof(other));
+
+			var theirs = new HashSet<T>(other, Comparer);
+
+			if (Count <= theirs.Count)
+				return false;
+
+			foreach (T item in theirs)
+				if (!Contains(item))
+					return false;
+
+			return true;
+		}
+
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+
+		void ICollection<T>.Add(T item) => AddItem(item);
+	}
+}
